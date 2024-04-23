2d570de8d7f907c1ed15ddd100fbb2af4c195269
vfliagin <93093512+vfliagin@users.noreply.github.com>
2024-04-23 15:59:38 +0300

    Update convolution2.py

--- convolution2.py
+++ convolution2.py
@@ -2,10 +2,15 @@ import cv2
 import numpy as np
 
 class Convolution:
-    def __call__(self, in_data:np.ndarray, filters:np.ndarray):
+    def __call__(self, in_data:np.ndarray, filters:np.ndarray, B, stride_col = 1, stride_row = 1):
 
         self.in_data = in_data
         self.filters = filters
+
+        self.stride_col = stride_col
+        self.stride_row = stride_row
+
+        self.B = B
 
         self.H, self.W, self.C = self.in_data.shape 
 
@@ -13,13 +18,16 @@ class Convolution:
 
         self.O = 0
 
-        for dim in [self.M, self.W, self.H]:
+        output_cols = 1 + (self.W - self.S) // self.stride_col
+        output_rows = 1 + (self.H - self.R) //self.stride_row
+
+        for dim in [self.M, output_cols, output_rows]:
             self.O = [self.O]*dim
 
         self.O = np.array(self.O)
 
-        for x in range(self.H - self.R + 1):
-            for y in range(self.W - self.S + 1):
+        for x in range(output_rows):
+            for y in range(output_cols):
                 for m in range(self.M):
                     self.compute_convolution_for_point(m, x, y)
         
@@ -33,7 +41,7 @@ class Convolution:
         for i in range(self.R):
             for j in range(self.S):
                 for k in range(self.C):
-                    self.O[x][y][m] += self.in_data[x+i][y+j][k] * self.filters[m][k][i][j]
+                    self.O[x][y][m] += self.B[m] + self.in_data[self.stride_row*x+i][self.stride_col*y+j][k] * self.filters[m][k][i][j]
                     
 if __name__ == '__main__':
 
@@ -43,11 +51,13 @@ if __name__ == '__main__':
     print('New shape: ', orig_img.shape)
     C = orig_img.shape[2]
     print('Channels: ', C)
+    B = [0] * C
     sharpener = np.array([[[0, -1, 0], [-1, 5, -1], [0, -1, 0]]] * C)
     img_filters.append(sharpener)
     img_filters = np.array(img_filters)
     con = Convolution()
-    res = con(orig_img, img_filters)
+    res = con(orig_img, img_filters, B)
     cv2.imwrite('port_sharpened.jpg', res)
+    
     
    
